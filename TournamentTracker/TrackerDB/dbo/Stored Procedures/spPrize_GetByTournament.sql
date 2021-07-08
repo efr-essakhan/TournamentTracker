0aa93d4df0de3d1d7a699729a4febcb37246ce66
Essa Khan <49402709+essakh@users.noreply.github.com>
2021-07-08 15:19:38 +0100

    Created stored procedure files

---
+++ TournamentTracker/TrackerDB/dbo/Stored Procedures/spPrize_GetByTournament.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spPrize_GetByTournament]
+	@TournamentId int
+AS
+begin
+	SET NOCOUNT ON;
+
+	select p.*
+	from dbo.Prize p
+	inner join dbo.TournamentPrize t on p.Id = t.PrizeId
+	where t.TournamentId = @TournamentId;
+
+end
